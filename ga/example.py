985c018a8eac5e0754d5483d84f1bc83f52df099
Amir <amironsoft@mail.ru>
2019-09-11 13:08:49 +0300

    add boston example

--- ga/example.py
+++ ga/example.py
@@ -10,7 +10,7 @@ def get_data(dataset_name):
         dataset = load_boston()
         X, y = dataset.data, dataset.target
     elif dataset_name == 'robc':
-        ifile_path = r'C:\HOME\robocats\data\train_data.csv'
+        ifile_path = r'C:/HOME/robc/data/train_data.csv'
         dataset = pd.read_csv(ifile_path)
         y = dataset.pop('target')
         X = dataset
@@ -19,7 +19,7 @@ def get_data(dataset_name):
 
 @timeit
 def main():
-    dataset_name = 'robc'
+    dataset_name = 'boston'
     X, y = get_data(dataset_name)
 
     est_name = 'lasso'
@@ -27,13 +27,22 @@ def main():
                 'linreg': LinearRegression()}
 
     est = est_dict[est_name]
-    features_scores = get_scores_df(est_name, est, X, y, features_selectors=('RFE', ))
+    features_scores = get_scores_df(est_name, est, X, y)
     print(features_scores.to_string())
 
 
 if __name__ == '__main__':
     main()
 
+# boston
+#   est_name    name    mse  rmse  feat_count            time
+# 0    lasso    init  35.53  5.96          13        00:00:00
+# 1    lasso     RFE  35.53  5.96          13 00:00:00.058021
+# 2    lasso      RF  38.42  6.20           9 00:00:01.219316
+# 3    lasso  boruta  38.42  6.20           9 00:00:10.591762
+# 4    lasso     gen  33.15  5.76           9 00:00:06.891801
+
+# robc
 #   est_name  name     mse       rmse  feat_count            time
 # 0    lasso  init  104.73  10.233768         147        00:00:00
 # 1    lasso   RFE   43.95   6.629480           2 00:01:36.677027
